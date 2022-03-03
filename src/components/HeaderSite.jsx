69fa3289160dd28caac8e8501e65836e40d85847
braz99 <fabriciobrazil99@gmail.com>
2022-03-03 12:38:03 -0300

    Estiliza o header

--- src/components/HeaderSite.jsx
+++ src/components/HeaderSite.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "../styles/components/header-s.css";
 
 function HeaderSite() {
   return (
@@ -10,11 +11,12 @@ function HeaderSite() {
       <nav className="nav-s">
         <ul className="menu">
           <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
             <Link to="/cart">
-              <img src="./assets/cart-icon.svg" alt="Ícone do carrinho" />
+              <img
+                src="./assets/cart-icon.svg"
+                alt="Ícone do carrinho"
+                className="icon-cart"
+              />
             </Link>
           </li>
         </ul>
--- src/components/MainCarrinho.jsx
+++ src/components/MainCarrinho.jsx
@@ -1,32 +1,35 @@
-import React, { useEffect } from 'react';
-import listar from '../logica_app/listar_carrinho';
-import somarTotal from '../logica_app/somar_total';
-import verificar from '../logica_app/verificar_vazio';
+import React, { useEffect } from "react";
+import listar from "../logica_app/listar_carrinho";
+import somarTotal from "../logica_app/somar_total";
+import verificar from "../logica_app/verificar_vazio";
 
 function MainCarrinho() {
+  useEffect(() => listar());
 
-    useEffect(() => listar())
+  useEffect(() => somarTotal());
 
-    useEffect(() => somarTotal())
+  return (
+    <main>
+      <ul id="area_carrinho"></ul>
 
-    return (
-        <main id='main_carrinho'>
+      <div id="secao_preco">
+        <h3>
+          Total sem frete: <span id="subtotal"> R$ 0,00 </span>
+        </h3>
+        <h2>
+          Total dos produtos: <span id="total"> R$ 0,00</span>
+        </h2>
 
-            <ul id='area_carrinho'>
-
-            </ul>
-
-            <div id='secao_preco'>
-
-                <h3>Total sem frete: <span id='subtotal'> R$ 0,00 </span></h3>
-                <h2>Total dos produtos: <span id='total'> R$ 0,00</span></h2>
-
-                <button onClick={() => {verificar()}}>Finalizar compra</button>
-            </div>
-
-        </main>
-    )
-
+        <button
+          onClick={() => {
+            verificar();
+          }}
+        >
+          Finalizar compra
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default MainCarrinho;
