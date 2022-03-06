b0af246f324545a93a7705a374ab5f3875c99ca6
braz99 <fabriciobrazil99@gmail.com>
2022-03-06 11:24:32 -0300

    Limpa o código, removendo a lógica anterior

--- src/logica_app/remover.js
+++
@@ -1,7 +0,0 @@
-function remover(id, nome) {
-    localStorage.setItem(id, JSON.stringify({'nome': nome, 'status': 'remover'}))
-    alert(nome + '  removido do carrinho!')
-    document.location.reload()
-}
-
-export default remover;
